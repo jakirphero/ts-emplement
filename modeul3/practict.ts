133f0e6d592d5192779f7e72fd395e679419f80e
jakir <mdjakirhossain2235@gmail.com>
2025-05-06 09:18:37 +0600

    polymorphism

--- modeul3/practict.ts
+++ modeul3/practict.ts
@@ -21,48 +21,38 @@
     // inheritance ------ parat to child call
 
     // class company 
-    type Department = {
-        name: string
+
+    // class Person
+    class Person {
+        public name: string = ''
+        public role: string = ''
     }
 
-    type Employee = {
-        name: string
-        age: number
-    }
-
-    class TeslaCompany {
-        private static role = "Admin"
-        private readonly credentials: string = ''
-        private departments: (Department)[] = []
-        private employees: (Employee)[] = []
-
-        constructor(cred: string) {
-            this.credentials = cred
+    class basketballPlayer extends Person {
+        public setName(name: string) {
+            this.name = name
+            this.role = 'BasketBall Player'
         }
-
-        addDepartment(value: Department) {
-            this.departments = [...this.departments, value]
-        }
-
-        addEmployee(value: Employee) {
-            this.employees = [...this.employees, value]
+        public getName() {
+            return `User name: ${this.name} Role: ${this.role}`
         }
     }
 
-    class TeslaEmployee extends TeslaCompany {
-        private new_employee: Employee = { name: '', age: 0 }
-
-        public setName(name: Employee): void {
-            this.new_employee = name
-
+    class golfPlayer extends Person {
+        public setName(name: string) {
+            this.name = name
+            this.role = 'Golf Player'
+        }
+        public getName() {
+            return `User name: ${this.name} Role: ${this.role}`
         }
     }
 
-    const newTeslaEmployee = new TeslaEmployee('123456')
-    newTeslaEmployee.setName({ name: 'Kevin Odongo', age: 36 })
-    newTeslaEmployee.addDepartment({ name: 'Finance' })
-    newTeslaEmployee.addEmployee({ name: 'Kevin Odongo', age: 36 })
-    console.log(newTeslaEmployee);
-
+    const person1 = new basketballPlayer();
+    const person2 = new golfPlayer();
+    person1.setName('jk dhone');
+    person2.setName('hero ji')
+    console.log(person1.getName());
+    console.log(person2.getName());
 
 }
